36af46e6d51ce796956c57cdfef948a3e13a1827
Tanvir <zishantanvir05@gmail.com>
2024-09-21 13:01:46 +0600

    confirm validation

--- js/addMoney.js
+++ js/addMoney.js
@@ -14,6 +14,13 @@ document
         const addMoney = getInputFieldValueById("input-add-money");
         const pinNumber = getInputFieldValueById("input-pin-number");
 
+        //number validation
+        if(isNaN(addMoney)){
+            alert("Enter Number ??????")
+            return;
+        }
+
+
         //wrong way to verify . do not try it at your serious website
         if (pinNumber === 1234) {
             const balance = getTextFieldValueById("account-balance");
@@ -22,6 +29,9 @@ document
 
             // add to transction history
             const p = document.createElement("p");
+            p.classList.add('bg-blue-300')
+            p.classList.add('p-3')
+            p.classList.add('text-center')
             p.innerText = `Added ${addMoney} Tk. New Balancec ${newBalance}`;
 
             //should be a common function
--- js/cashOut.js
+++ js/cashOut.js
@@ -5,8 +5,31 @@ document.getElementById('btn-cash-out').addEventListener('click', function (even
     const pin = getInputFieldValueById('input-cash-out-pin');
     const currentBalance = getTextFieldValueById('account-balance');
     const updateBalance = currentBalance - cashOut;
+
+    //number validation
+    if(isNaN(cashOut)){
+        alert("Enter Number ??????")
+        return;
+    }
+    if(currentBalance < cashOut){
+        alert("Not enough Money ")
+        return;
+    }
+
+
     if(pin === 1234){
         document.getElementById('account-balance').innerText = updateBalance;
+        
+        //add to transaction history
+        const div =document.createElement('div')
+        div.classList.add('bg-yellow-200')
+        div.innerHTML = `
+        <h1 class="text-3xl text-center font-bold">Cashout History</h1>
+        <p class="text-center font-bold">${cashOut} withdraw and new balance ${updateBalance}</p>
+        `
+        const newDiv = document.getElementById('transaction-history')
+        newDiv.appendChild(div)
+
     }
     else{
         alert("Falied to cash out . Try again")
